f90be89264a718989437c71531b2769da5c45151
Jonathan Price <jonprice0@gmail.com>
2021-09-19 18:10:12 -0400

    finished evaluateAnswer() function and added highscore html and endQuiz() function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 var timeLeft = 75;
-var score = timeLeft;
+var score = 0;
 var i = 0
-
 var questionArr = [
     {
         "question": "Commonly used data types do NOT include:",
@@ -17,52 +16,105 @@ var questionArr = [
         "A2": "2. curly brackets",
         "A3": "3. paranthesis",
         "A4": "4. square brackets",
-        "answer": "3. paranthesis"
+        "answer": "3. parenthesis"
+    },
+    {
+        "question": "Arrays in Javascript can be used to store ________.",
+        "A1": "1. numbers and strings",
+        "A2": "2. other arrays",
+        "A3": "3. booleans",
+        "A4": "4. all of the above",
+        "answer": "4. all of the above"
+    },
+    {
+        "question": "String values must be enclosed with ________ when being assigned to variables.",
+        "A1": "1. commas",
+        "A2": "2. curly brackets",
+        "A3": "3. quotes",
+        "A4": "4. parenthesis",
+        "answer": "3. quotes"
+    },
+    {
+        "question": "A very useful tool used during development and debugging for printing content to the debugger is ________:",
+        "A1": "1. Javascript",
+        "A2": "2. terminal/bash",
+        "A3": "3. for loops",
+        "A4": "4. console.log",
+        "answer": "4. console.log"
     }
-]
-var renderQuestion = function() {
-    document.querySelector("p").remove();
-    document.querySelector("#start").remove();
+];
+var renderFirstQuestion = function() {
     document.querySelector("h1").innerHTML = questionArr[i].question;
     var btn1 = document.createElement("BUTTON"); 
-    btn1.innerHTML = questionArr[i].A1;                 
+    btn1.innerHTML = questionArr[i].A1;
+    btn1.className="choices";
+    btn1.id="btn1";            
     document.body.appendChild(btn1);
     var btn2 = document.createElement("BUTTON"); 
-    btn2.innerHTML = questionArr[i].A2;                 
+    btn2.innerHTML = questionArr[i].A2;  
+    btn2.className="choices"; 
+    btn2.id="btn2";              
     document.body.appendChild(btn2);
     var btn3 = document.createElement("BUTTON"); 
-    btn3.innerHTML = questionArr[i].A3;                 
+    btn3.innerHTML = questionArr[i].A3;  
+    btn3.className="choices"; 
+    btn3.id="btn3";              
     document.body.appendChild(btn3);
     var btn4 = document.createElement("BUTTON"); 
-    btn4.innerHTML = questionArr[i].A4;                 
-    document.body.appendChild(btn4); 
+    btn4.innerHTML = questionArr[i].A4; 
+    btn4.className="choices";
+    btn4.id="btn4";                
+    document.body.appendChild(btn4);
     return;
 };
-var evaluateAnswer = function() {
-    console.log("Hello!");
-    return;
-};
-
-var endQuiz = function() {
-
-};
-
-var countdown = function() {
+var renderNextQuestion = function() {
     if (i < questionArr.length) {
-    renderQuestion();
-    i++;
-    document.querySelector("button").addEventListener("click", evaluateAnswer);
+    document.querySelector("h1").innerHTML = questionArr[i].question;
+    document.querySelector("#btn1").innerHTML = questionArr[i].A1;
+    document.querySelector("#btn2").innerHTML = questionArr[i].A2;
+    document.querySelector("#btn3").innerHTML = questionArr[i].A3;
+    document.querySelector("#btn4").innerHTML = questionArr[i].A4;
+    return;
+    } else {
+        endQuiz();
     }
-    else {
-    endQuiz();
 };
-
-    setInterval(function(){
-        if (timeLeft >= 0) {
-        document.querySelector("#timer").innerHTML = "Timer: " + timeLeft;
-        timeLeft--;
-    }
-    }, 1000);
+var evaluateAnswer = function(event) {
+    if (event.target.textContent === questionArr[i].answer) {
+        i++;
+        renderNextQuestion();
+        return;
+    } else {
+        timeLeft = timeLeft - 10;
+        i++;
+        renderNextQuestion();
+        return;
+    }   
 };
-
-document.querySelector("#start").addEventListener("click", countdown);
+var endQuiz = function() {
+    document.querySelector("h1").remove();
+    document.querySelector("#btn1").remove();
+    document.querySelector("#btn2").remove();
+    document.querySelector("#btn3").remove();
+    document.querySelector("#btn4").remove();
+    localStorage.setItem("score", timeLeft);
+};
+var quizMode = function() {
+        document.querySelector("p").remove();
+        document.querySelector("#start").remove(); 
+        renderFirstQuestion();
+        document.querySelector("#btn1").addEventListener("click", evaluateAnswer);
+        document.querySelector("#btn2").addEventListener("click", evaluateAnswer);
+        document.querySelector("#btn3").addEventListener("click", evaluateAnswer);
+        document.querySelector("#btn4").addEventListener("click", evaluateAnswer);
+    var countdown = setInterval(function() {
+            if (timeLeft > 0 && i < questionArr.length) {
+                document.querySelector("#timer").innerHTML = "Timer: " + timeLeft;
+                timeLeft--;
+            } else {
+                clearInterval(countdown);
+                endQuiz();
+            }
+        }, 1000);
+};
+document.querySelector("#start").addEventListener("click", quizMode);
