01884b337c77645a3c171de164defcae991c52b8
Jonathan Price <jonprice0@gmail.com>
2021-09-19 13:40:50 -0400

    fleshed out renderQuestion() function and added test evaluateAnswer() function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var timeLeft = 75;
 var score = timeLeft;
+var i = 0
 
 var questionArr = [
     {
@@ -20,12 +21,42 @@ var questionArr = [
     }
 ]
 var renderQuestion = function() {
-    for (i = 0; i < questionArr.length; i--) {
-        document.querySelector("h1").innerHTML = "Hello!";
-    }
+    document.querySelector("p").remove();
+    document.querySelector("#start").remove();
+    document.querySelector("h1").innerHTML = questionArr[i].question;
+    var btn1 = document.createElement("BUTTON"); 
+    btn1.innerHTML = questionArr[i].A1;                 
+    document.body.appendChild(btn1);
+    var btn2 = document.createElement("BUTTON"); 
+    btn2.innerHTML = questionArr[i].A2;                 
+    document.body.appendChild(btn2);
+    var btn3 = document.createElement("BUTTON"); 
+    btn3.innerHTML = questionArr[i].A3;                 
+    document.body.appendChild(btn3);
+    var btn4 = document.createElement("BUTTON"); 
+    btn4.innerHTML = questionArr[i].A4;                 
+    document.body.appendChild(btn4); 
+    return;
+};
+var evaluateAnswer = function() {
+    console.log("Hello!");
+    return;
+};
+
+var endQuiz = function() {
+
+};
 
 var countdown = function() {
+    if (i < questionArr.length) {
     renderQuestion();
+    i++;
+    document.querySelector("button").addEventListener("click", evaluateAnswer);
+    }
+    else {
+    endQuiz();
+};
+
     setInterval(function(){
         if (timeLeft >= 0) {
         document.querySelector("#timer").innerHTML = "Timer: " + timeLeft;
